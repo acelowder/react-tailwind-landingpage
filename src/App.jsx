e50f133bfa10d84c564f31873a6f579791f2ef9e
Ace <ace.lowder@gmail.com>
2024-05-31 11:00:26 -0700

    Extract sections into components

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,41 @@
+import {
+  Hero,
+  PopularProducts,
+  SuperQuality,
+  Services,
+  SpecialOffer,
+  CustomerReviews,
+  Subscribe,
+  Footer,
+} from "./sections";
+
 const App = () => (
   <main className="relative">
     Nav
-    <section>Hero</section>
-    <section className="padding">PopularProducts</section>
-    <section className="padding">SuperQuality</section>
-    <section className="padding">Services</section>
-    <section className="padding">SpecialOffer</section>
-    <section className="padding bg-pale-blue">CustomerReviews</section>
-    <section className="padding">Subscribe</section>
-    <section className="padding bg-black pb-8">Footer</section>
+    <section>
+      <Hero />
+    </section>
+    <section className="padding">
+      <PopularProducts />
+    </section>
+    <section className="padding">
+      <SuperQuality />
+    </section>
+    <section className="padding">
+      <Services />
+    </section>
+    <section className="padding">
+      <SpecialOffer />
+    </section>
+    <section className="padding bg-pale-blue">
+      <CustomerReviews />
+    </section>
+    <section className="padding">
+      <Subscribe />
+    </section>
+    <section className="padding bg-black pb-8">
+      <Footer />
+    </section>
   </main>
 );
 
