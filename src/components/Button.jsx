9070f04deafc9a20c061cd0207ef99a53e1895d6
Ace <ace.lowder@gmail.com>
2024-06-03 18:12:10 -0700

    Add button component

---
+++ src/components/Button.jsx
@@ -0,0 +1,14 @@
+const Button = ({ label, iconUrl }) => {
+  return (
+    <button className="flex justify-center items-center gap-2 px-7 py-4">
+      {label}
+      <img
+        src={iconUrl}
+        alt="arrow right icon"
+        className="ml-2 rounded-full w-5 h-5"
+      />
+    </button>
+  );
+};
+
+export default Button;
