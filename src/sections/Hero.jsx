fffcbd0adf4803dace02d62b412db9d93021ca3d
Ace <ace.lowder@gmail.com>
2024-06-04 15:53:24 -0700

    Add components index.js file

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 
-import Button from "../components/Button";
-import ShoeCard from "../components/ShoeCard";
+import { Button, ShoeCard } from "../components";
 import { statistics, shoes } from "../constants";
 import { arrowRight } from "../assets/icons";
 import { bigShoe1 } from "../assets/images";
@@ -16,8 +15,9 @@ const Hero = () => {
     >
       <div className="relative xl:w-2/5 flex flex-col justify-center items-start w-full max-xl:padding-x pt-28">
         <p className="text-xl font-montserrat text-coral-red">
-          Our Summer Collection
+          Our Summer collections
         </p>
+
         <h1 className="mt-10 font-palanquin text-8xl max-sm:text-[72px] max-sm:leading-[82px] font-bold">
           <span className="xl:bg-white xl:whitespace-nowrap relative z-10 pr-10">
             The New Arrival
@@ -29,7 +29,9 @@ const Hero = () => {
           Discover stylish Nike arrivals, quality comfort, and innovation for
           your active life.
         </p>
+
         <Button label="Shop now" iconUrl={arrowRight} />
+
         <div className="flex justify-start items-start flex-wrap w-full mt-20 gap-16">
           {statistics.map((stat) => (
             <div key={stat.label}>
@@ -50,6 +52,7 @@ const Hero = () => {
           height={500}
           className="object-contain relative z-10"
         />
+
         <div className="flex sm:gap-6 gap-4 absolute -bottom-[5%] sm:left-[10%] max-sm:px-6">
           {shoes.map((shoe) => (
             <div key={shoe}>
