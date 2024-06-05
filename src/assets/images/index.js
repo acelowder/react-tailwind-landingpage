28da49ab658f54d56c18400c6355406627c3e6a1
Ace <ace.lowder@gmail.com>
2024-06-05 16:39:45 -0700

    Update popular products section

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -9,6 +9,11 @@ import thumbnailShoe1 from "./thumbnail-shoe1.svg";
 import thumbnailShoe2 from "./thumbnail-shoe2.svg";
 import thumbnailShoe3 from "./thumbnail-shoe3.svg";
 
+import shoe4 from './shoe4.svg'
+import shoe5 from './shoe5.svg'
+import shoe6 from './shoe6.svg'
+import shoe7 from './shoe7.svg'
+
 export {
   bigShoe1,
   bigShoe2,
@@ -18,4 +23,8 @@ export {
   thumbnailShoe1,
   thumbnailShoe2,
   thumbnailShoe3,
+  shoe4,
+  shoe5,
+  shoe6,
+  shoe7,
 };
