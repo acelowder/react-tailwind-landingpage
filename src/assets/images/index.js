6d519025beef9806d80d937c969b42012a024bb3
Ace <ace.lowder@gmail.com>
2024-06-05 18:25:22 -0700

    Add super quality image

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -13,6 +13,7 @@ import shoe4 from './shoe4.svg'
 import shoe5 from './shoe5.svg'
 import shoe6 from './shoe6.svg'
 import shoe7 from './shoe7.svg'
+import shoe8 from './shoe8.svg'
 
 export {
   bigShoe1,
@@ -27,4 +28,5 @@ export {
   shoe5,
   shoe6,
   shoe7,
+  shoe8,
 };
--- src/sections/SuperQuality.jsx
+++ src/sections/SuperQuality.jsx
@@ -1,3 +1,4 @@
+import { shoe8 } from "../assets/images";
 import { Button } from "../components";
 
 const SuperQuality = () => {
@@ -8,7 +9,7 @@ const SuperQuality = () => {
     >
       <div className='flex flex-1 flex-col'>
         <h2 className='font-palanquin capitalize text-4xl lg:max-w-lg font-bold'>
-          We Provide You
+          We Provide
           <span className='text-coral-red'> Super </span>
           <span className='text-coral-red'>Quality </span> Shoes
         </h2>
@@ -23,6 +24,16 @@ const SuperQuality = () => {
         <div className='mt-11'>
           <Button label='View details' />
         </div>
+      </div>
+
+      <div className='flex-1 flex justify-center items-center'>
+        <img
+          src={shoe8}
+          alt='product detail'
+          width={570}
+          height={522}
+          className='object-contain'
+        />
       </div>
     </section>
   );
